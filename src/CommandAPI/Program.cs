aba4c18acfa01c55b432638e35d76865bf6c1bd5
kemal genc <kemalgenc@gmail.com>
2022-04-29 14:30:48 +0300

    Added Dto

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,6 +1,7 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 var connStringBuilder = new NpgsqlConnectionStringBuilder();
@@ -9,6 +10,7 @@ connStringBuilder.Username = builder.Configuration["UserID"];
 connStringBuilder.Password = builder.Configuration["Password"];
 builder.Services.AddControllers();
 builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(connStringBuilder.ConnectionString));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 //builder.Services.AddScoped<ICommandRepo, MockCommandRepo>();
 builder.Services.AddScoped<ICommandRepo, SqlCommandRepo>();
 var app = builder.Build();
